25f50bf0313bb00baa4f5f60cfff1d0e2b1ef83d
Camoen <camoenvw@gmail.com>
2018-11-14 22:00:39 -0500

    Add bootstrap functionality, new pages, custom stylesheets

--- Sites/complaintapp/app/controllers/posts_controller.rb
+++ Sites/complaintapp/app/controllers/posts_controller.rb
@@ -1,13 +1,33 @@
 class PostsController < ApplicationController
 	def index
-		p = ActiveRecord::Base.establish_connection
-		c = p.connection
-		@results = c.execute("select * from camoen.complaint where rownum <= 10")
-		while r = @results.fetch()
-			puts r.join(',')
-		end
-		@results.close
 	end
 	
+	def query_directory
+	end
+
+	def query1
+		p = ActiveRecord::Base.establish_connection
+		c = p.connection
+
+		# @results = c.execute("select * from camoen.complaint where rownum <= 10")
+		# while r = @results.fetch()
+		# 	puts r.join(',')
+		# end
+		# @results.close
+
+		@results = c.exec_query("select distinct name from camoen.complaint where rownum <= 10").to_a
+		# results.each do |result|
+		#   	puts result[0]
+		# end
+
+		# for result in @results
+		# 	result.name #|  posting.time | posting.salary 
+		# end
+
+		# def index
+		#   	@telephone_records = TelephoneRecord.all
+		# end
+		
+	end
 
 end
--- Sites/complaintapp/config/routes.rb
+++ Sites/complaintapp/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  get 'posts/query_directory'
+  get 'posts/query1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
